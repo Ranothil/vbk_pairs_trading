cbfa7d068ac76870221fd4586da0cdea32af3e23
Ranothil <zekelegge2@gmail.com>
2020-11-02 11:39:54 -0500

    Initial commit

---
+++ find_pairs.py
@@ -0,0 +1,9 @@
+# find_pairs.py will locate suitable pair trading opportunities among VBK constituents
+# through correlation analysis
+
+import pandas as pd
+import numpy as np
+
+# import vbk constituent price data from data directory into pandas dataframes
+def import_data():
+    pass
